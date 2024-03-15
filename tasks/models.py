ef0666a34fe6cd01735272a6c385e478cef19501
Karol Braga <karol.fbr@gmail.com>
2024-03-15 13:11:52 -0300

    Refactor Task model for improved readability

--- tasks/models.py
+++ tasks/models.py
@@ -2,13 +2,18 @@ from django.db import models
 from django.db import models
 
 class Task(models.Model):
-    STATUS = (
+    STATUS_CHOICES = (
         ('todo', 'To Do'),
         ('in_progress', 'In Progress'),
         ('done', 'Done'),
     )
+    PRIORITY_CHOICES = (
+        (1, 'Low'),
+        (2, 'Medium'),
+        (3, 'High'),
+    )
     title = models.CharField(max_length=100)
     description = models.TextField(null=True)
-    status = models.CharField(max_length=12, choices=STATUS, default='in_progress')
-    priority = models.IntegerField()
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='in_progress')
+    priority = models.IntegerField(choices=(PRIORITY_CHOICES), default=1)
     created_at = models.DateTimeField(auto_now_add_date=True)
