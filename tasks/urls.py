884b4a0b7023853480f60edb465053252fa5bfbf
Karol Braga <karol.fbr@gmail.com>
2024-03-15 17:09:17 -0300

    Implemented TaskRetrieveUpdateDestroy API view for retrieving, updating, and deleting Task instances

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("tasks/", views.TaskListCreate.as_view(), name="task-view-create")
+    path("tasks/", views.TaskListCreate.as_view(), name="task-view-create"),
+    path("tasks/<int:pk>/", views.TaskRetrieveUpdateDestroy.as_view(), name="update")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
 
 class TaskListCreate(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    
+class TaskRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field = "pk"
